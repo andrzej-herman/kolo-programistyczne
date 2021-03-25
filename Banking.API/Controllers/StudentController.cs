18536df712a38f2c13f26f5791b7b01237c6eded
andrzej-herman <andrzejek.herman@gmail.com>
2021-03-25 20:40:36 +0100

    Add project

---
+++ Banking.API/Controllers/StudentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banking.API.Controllers
+{
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+
+        [HttpGet("/api/students")]
+        public IActionResult GetStudents()
+        {
+            return View();
+        }
+
+
+
+    }
+}
